a1b355bce316a6cfd0c1dcf6ab905710c0781fec
kooBH <k01032633280@gmail.com>
2019-07-30 13:32:15 +0900

    .profile

---
+++ append.sh
@@ -0,0 +1,10 @@
+
+alias python=python3
+
+alias gs='git status'
+alias ga='git add'
+alias gc='git commit -m '
+alias gps='git push origin'
+alias gpl='git pull origin'
+
+
--- set-up.sh
+++ set-up.sh
@@ -3,6 +3,8 @@
 # Packages
 sudo apt-get --assume-yes install build-essential cmake
 sudo apt-get --assume-yes install git-core
+sudo apt-get -y install clang
+sudo apt-get -y install build-essential cmake
 
 # Python 
 sudo apt-get --assume-yes install python-dev python3-dev
@@ -19,16 +21,9 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 sudo cp -r .vim/* ~/.vim/
 sudo cp .vimrc ~/.vimrc
 
-git clone https://github.com/Valloric/YouCompleteMe.git
+./ycm.sh
 
-sudo apt-get --asume-yes install clang
-sudo aptg-et --asume-yes install python
-cd YouCompleteMe
-git submodule update --init --recursive
-./install.py --clang-completer --system-libclang
-./install.sh
-cd ..
-mv YouCompleteMe ~/.vim/bundle/
+# echo $(which python)
 
 ### tmux
 
@@ -42,6 +37,7 @@ cp .tmux.conf ~/.tmux.conf
 
 echo "prefix + 'I' at tmux to install tmux-resurrect"
 
+cat append.sh >> ~/.profile
 
 ###
 
