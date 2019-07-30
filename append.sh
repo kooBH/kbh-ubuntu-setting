7456f414f3812a520ed66cd659134fb417be935b
kooBH <k01032633280@gmail.com>
2019-07-30 13:35:15 +0900

    amend

--- append.sh
+++ append.sh
@@ -4,6 +4,7 @@ alias python=python3
 alias gs='git status'
 alias ga='git add'
 alias gc='git commit -m '
+alias gca='git commit --amend '
 alias gps='git push origin'
 alias gpl='git pull origin'
 
