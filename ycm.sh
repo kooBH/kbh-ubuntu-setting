2d53d0b55a2d2e6e329b0db59345fea16068a370
kbh <nine4409@sogang.ac.kr>
2020-03-17 14:28:30 +0900

    less system-libclang

--- ycm.sh
+++ ycm.sh
@@ -8,7 +8,7 @@ sudo apt-get --asume-yes install cmake
 sudo apt-get --asume-yes install python3-dev
 cd YouCompleteMe
 git submodule update --init --recursive
-python3 install.py --clang-completer --system-libclang
+python3 install.py --clang-completer
 #./install.sh
 cd ..
 mv YouCompleteMe ~/.vim/bundle/
