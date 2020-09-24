75c7e3675e48ccbf22252dfadb6058ec4f062654
koobh <nine4409@u.sogang.ac.kr>
2020-09-24 16:57:02 +0900

    fix typo

--- ycm.sh
+++ ycm.sh
@@ -3,7 +3,7 @@
 git clone https://github.com/Valloric/YouCompleteMe.git
 
 sudo apt-get --asume-yes install clang
-sudo aptg-et --asume-yes install python
+sudo apt-get --asume-yes install python
 cd YouCompleteMe
 git submodule update --init --recursive
 ./install.py --clang-completer --system-libclang
