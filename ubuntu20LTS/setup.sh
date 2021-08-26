a168cf2b75ea34fa7e423765646b208321fb6bc0
koobh <nine4409@sogang.ac.kr>
2021-08-26 20:47:23 +0900

    for mount

--- ubuntu20LTS/setup.sh
+++ ubuntu20LTS/setup.sh
@@ -7,6 +7,12 @@ sudo apt-get install -y tmux
 
 sudo apt-get install -y build-essential cmake
 
+## to mount windows
+sudo apt-get install nfs-common
+
+## to mount other server
+sudo apt-get install sshfs
+
 
 sudo apt-get install -y vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
