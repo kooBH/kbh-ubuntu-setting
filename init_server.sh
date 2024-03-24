47fd0ea7d77769b802df7ddc83143ac9cbe61542
KooBH <nine4409@sogang.ac.kr>
2024-03-24 17:56:13 +0900

    Create init_server.sh

---
+++ init_server.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+
+sudo apt install openssh-server
+sudo apt install sshfs
+sudo apt install nfs-common
+
+sudo apt install git-core
+sudo apt install vim
+sudo apt install gcc
+sudo apt install g++
+
+sudo apt install htop
+sudo apt install tmux
